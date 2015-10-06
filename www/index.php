b6f89dc2c807b5ae42d5b94b38a576eff84a223b
Mitchfizz05 <me@mitchfizz05.net>
2015-10-07 02:00:52 +1030

    Basic webpage design in place

--- www/index.php
+++ www/index.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cloudflare Check</title>
 
-// 
+        <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    </head>
+    <body>
+        <div id="header">
+            <span>Does it use</span>
+            <img src="assets/img/cloudflare-logo.png" alt="Cloudflare" id="cloudflare-logo">
+            <span>?</span>
+        </div>
+        <div id="content">
+            <input type="text" id="urlbox">
+        </div>
+    </body>
+</html>
